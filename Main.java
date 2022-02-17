2d4a0c7da6631b7182ea8deea3d5504c1935011a
colin-anderson-g <colin.anderson.g@gmail.com>
2022-02-16 19:37:54 -0500

    Write solution for sortNumbersUp()

--- Main.java
+++ Main.java
@@ -1,15 +1,19 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Main {
 
 
     public static void main(String[] args) {
+        ArraySort arraySortObj = new ArraySort();
 
 
         System.out.println("----- Original list of numbers -----");
-        //TODO: Print original numbers
+        System.out.println(Arrays.toString(arraySortObj.getNumbers()));
 
 
         System.out.println("----- Increasing list of numbers -----");
-        //TODO: Print numbers sorted up
+        System.out.println(Arrays.toString(arraySortObj.sortNumbersUp(arraySortObj.getNumbers())));
 
 
         System.out.println("----- Decreasing list of numbers -----");
