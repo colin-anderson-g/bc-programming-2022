350364e74b8738283a4e5d585e72e0e5ade3b67d
TripB1432 <92406175+TripB1432@users.noreply.github.com>
2022-02-26 20:21:15 -0500

    Finnished project 1 and 2 in euler

---
+++ com/company/Multiples.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class Multiples {
+
+    public static void main(String[] args) {
+
+        //What number it goes till.
+        int Loop = 1000;
+        int Sum = 0;
+        //Creates A integer for the Loop
+        int A = 0;
+        //Creates a Loop
+        while(A < Loop) {
+
+
+            if(A % 3 == 0 && A % 5 == 0) {
+
+                Sum += A;
+
+                //if false continue.
+            }else if(A % 3 == 0){
+                Sum += A;
+                //System.out.println(A);
+
+            }else if(A % 5 == 0){
+                Sum += A;
+                //System.out.println(A);
+            }
+            //Add 1 to A
+            A++;
+        }
+
+        System.out.println(Sum);
+    }
+}
+
