f66e0848b8ad8e6e77ed6bceebec863e2d848ee2
TripB1432 <92406175+TripB1432@users.noreply.github.com>
2022-03-08 21:02:48 -0500

    Push Trip_Project_eular

---
+++ Trip_Project_Euler/Even_Fibonacci_Numbers.java
@@ -0,0 +1,28 @@
+package Trip_Project_Euler;
+
+    public class Even_Fibonacci_Numbers{
+
+        public static void main(String[] args){
+
+            int A = 1;
+            int B = 2;
+            int tmp = 0;
+            int sum = 2;
+
+            //System.out.println(A);
+            //System.out.println(B);
+
+            while (tmp < 4_000_000){
+
+                tmp = A + B;
+
+                if (tmp % 2 == 0 && tmp < 4_000_000){
+                    sum += tmp;
+                }
+
+                A = B;
+                B = tmp;
+            }
+            System.out.println(sum);
+        }
+    }
